b95fe8bd7928037dab42a61d6967c20f3bd2867f
Boss <taneev@iswin.ru>
2018-06-10 15:15:11 +0300

    prepare plibrary for packagist

---
+++ DependencyInjection/BormExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony MakerBundle package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Iswin\Borm\DependencyInjection;
+
+use Symfony\Bundle\MakerBundle\DependencyInjection\CompilerPass\MakeCommandRegistrationPass;
+use Symfony\Bundle\MakerBundle\MakerInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension as BaseExtension;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
+class BormExtension extends BaseExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
